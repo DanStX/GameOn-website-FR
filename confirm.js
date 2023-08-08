32e8010f79875aaf4667d85b5c3de87790adaa8f
DanStX <nyoungsteve06@gmail.com>
2023-08-08 10:46:11 +0200

    firstname lastname fait

--- confirm.js
+++ confirm.js
@@ -43,11 +43,70 @@ const Errors = {
     e.preventDefault(); // Empêche le rechargement de la page par défaut
   });
 
+  // Les données doivent être saisies correctement :
   
-// Les données doivent être saisies correctement :
-// (1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
+  // (1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
+// ############################ FirstName ######################  
+
+// (1) Validation du champ "Prénom" (firstName)
+function firstConfirm() {
+    let first = document.getElementById("first");
+    let myErrorFirst = document.getElementById("errorFirst");
+    
+    // - si la valeur dans le champs est supérieur à 2 caractères et 
+    if (first.value.length < 2) {
+      // - on affiche un message d'erreur et le cadre du champs devient rouge
+      myErrorFirst.innerHTML = Errors.nameError;
+      first.style.border = "1px solid red";
+      myErrorFirst.style.color = "red";
+      return false;
+      
+      // - si la valeur saisie ne correspond pas aux conditions du regex avec la méthode test()
+    } else if (!nameRegex.test(first.value)) {
+      // - on affiche un message d'erreur et le cadre du champs devient rouge
+      myErrorFirst.innerHTML = Errors.nameRegexError;
+      first.style.border = "1px solid red";
+      myErrorFirst.style.color = "red";
+      return false;
+      
+    } else {
+        
+        myErrorFirst.innerHTML = "";
+        first.style.border = "1px solid white";
+        return true;
+    }
+}
+
 // (2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
-// (3) L'adresse électronique est valide.
+  // ############################ LastName ######################  
+  // (2) Validation du champ "Nom de famille" (lastName)
+  function lastConfirm() {
+    let last = document.getElementById("last");
+    let myErrorLast = document.getElementById("errorLast");
+    // - si la valeur dans le champs est supérieur à 2 caractères et 
+      if (last.value.length < 2) {
+        // - on affiche un message d'erreur et le cadre du champs devient rouge
+        myErrorLast.innerHTML = Errors.nameError;
+        last.style.border = "1px solid red";
+        myErrorLast.style.color = "red";
+        return false;
+      // - si la valeur saisie ne correspond pas aux conditions du regex avec la méthode test()
+      } else if (!nameRegex.test(last.value)) {
+        // - on affiche un message d'erreur et le cadre du champs devient rouge
+        myErrorLast.innerHTML = Errors.nameRegexError;
+        last.style.border = "1px solid red";
+        myErrorLast.style.color = "red";
+        return false;
+      } else {
+        myErrorLast.innerHTML = "";
+        last.style.border = "1px solid white";
+        return true;
+      }
+    }
+    
+    // (3) L'adresse électronique est valide.
+
+
 // (4) Pour le nombre de concours, une valeur numérique est saisie.
 // (5) Un bouton radio est sélectionné.
 // (6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
